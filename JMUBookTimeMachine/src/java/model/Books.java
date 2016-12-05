92c1738590b342d2341a5968ed9c80329731616e
massarmh <massarmh@134.126.141.147>
2016-12-05 18:17:51 -0500

    Created all .java files and actions

---
+++ JMUBookTimeMachine/src/java/model/Books.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author massarmh
+ */
+public class Books {
+    
+    private String title, author, publisher, coverPhoto;
+    private int edition;
+    
+    public Books (String title, String author, String publisher, String coverPhoto, int edition)
+    {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.coverPhoto = coverPhoto;
+        this.edition = edition;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getCoverPhoto() {
+        return coverPhoto;
+    }
+
+    public int getEdition() {
+        return edition;
+    }
+    
+}
---
+++ JMUBookTimeMachine/src/java/model/Books_For_SaleActions.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+import java.util.Date;
+
+/**
+ *
+ * @author massarmh
+ */
+public class Books_For_SaleActions {
+
+    public static boolean addBooks_For_Sale(Date postedDate, double price, int sold) {
+
+        BFS newBFS = new BFS(postedDate, price, sold);
+        return Books_For_SalePersistence.addBooks_For_Sale(newBFS);
+    }
+
+    public static boolean checkBooks_For_Sale(Date postedDate, int sold) {
+
+        return Books_For_SalePersistence.checkBooks_For_Sale(postedDate, sold);
+    }
+}
